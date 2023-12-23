bd5ff308cfadcb11931390829410b75dd55529e9
Mehdi RaTo <Mehdi.RaTo@Gmail.Com>
2023-12-23 14:00:23 +0330

    ...

---
+++ RefManager.js
@@ -0,0 +1,43 @@
+/**
+ * Reference Manager Class
+ */
+export class RefManager { }
+
+/**
+ * References Variable
+ */
+var objRefs = {};
+
+/**
+ * Set Reference
+ * @param {string} refName Reference Name
+ * @param {*} ref Reference
+ */
+RefManager.SetRef = function (refName, ref) {
+    objRefs[refName] = ref;
+}
+
+/**
+ * Get Reference
+ * @param {string} refName Reference Name
+ * @returns {*} Reference
+ */
+RefManager.GetRef = function (refName) {
+    return objRefs[refName];
+}
+
+/**
+ * Delete Reference
+ * @param {string} refName Reference Name
+ */
+RefManager.DelRef = function (refName) {
+    delete objRefs[refName];
+}
+
+/**
+ * Get References List
+ * @returns {string[]} 
+ */
+RefManager.GetRefsList = function () {
+    return Object.getOwnPropertyNames(objRefs);
+}
